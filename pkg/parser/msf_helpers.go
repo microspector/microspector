953cead2c5205ed6261171ec7b3d50779489f888
Christopher O'Connell <jwriteclub@gmail.com>
2019-05-16 23:22:18 +0300

    Add a basic pigeon based peg including string types.

---
+++ pkg/parser/msf_helpers.go
@@ -0,0 +1,73 @@
+package parser
+
+import (
+"strconv"
+"unicode/utf8"
+)
+
+// Helper functions
+
+// Unquote interprets s as a single-quoted, double-quoted,
+// or backquoted Go string literal, returning the string value
+// that s quotes. Unlike the version in strconv, this version
+// doesn't mind single quoted strings
+func strunquote(s string) (string, error) {
+	n := len(s)
+	if n < 2 {
+		return "", strconv.ErrSyntax
+	}
+	quote := s[0]
+	if quote != s[n-1] {
+		return "", strconv.ErrSyntax
+	}
+	s = s[1 : n-1]
+
+	if quote != '"' && quote != '\'' {
+		return "", strconv.ErrSyntax
+	}
+	if contains(s, '\n') {
+		return "", strconv.ErrSyntax
+	}
+
+	// Is it trivial? Avoid allocation.
+	if !contains(s, '\\') && !contains(s, quote) {
+		switch quote {
+		case '"':
+			return s, nil
+		case '\'':
+			r, size := utf8.DecodeRuneInString(s)
+			if size == len(s) && (r != utf8.RuneError || size != 1) {
+				return s, nil
+			}
+		}
+	}
+
+	var runeTmp [utf8.UTFMax]byte
+	buf := make([]byte, 0, 3*len(s)/2) // Try to avoid more allocations.
+	for len(s) > 0 {
+		c, multibyte, ss, err := strconv.UnquoteChar(s, quote)
+		if err != nil {
+			return "", err
+		}
+		s = ss
+		if c < utf8.RuneSelf || !multibyte {
+			buf = append(buf, byte(c))
+		} else {
+			n := utf8.EncodeRune(runeTmp[:], c)
+			buf = append(buf, runeTmp[:n]...)
+		}
+	}
+	return string(buf), nil
+}
+
+// contains reports whether the string contains the byte c.
+func contains(s string, c byte) bool {
+	for i := 0; i < len(s); i++ {
+		if s[i] == c {
+			return true
+		}
+	}
+	return false
+}
+
+
---
+++ pkg/parser/msf_test.go
@@ -0,0 +1,90 @@
+package parser
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+type tst struct {
+	input string
+	success bool
+	output interface{}
+}
+
+func doTest(s tst, opts ...Option) (func(t *testing.T)) {
+	return func(t *testing.T) {
+		//t.Parallel()
+		fmt.Printf("Testing \"%s\"\n", s.input)
+		iface, err := Parse("test", []byte(s.input), opts...)
+		if err != nil && s.success {
+			fmt.Printf("Error: %s\n", err.Error())
+			t.Fail()
+			return
+		} else if err == nil && !s.success {
+			fmt.Printf("Expecting an error and got none: %#v\n", iface)
+			t.Fail()
+			return
+		} else if err != nil && !s.success {
+			fmt.Printf("Successfully got an error\n")
+			return
+		}
+		typ := "nil"
+		if iface != nil {
+			typ = reflect.TypeOf(iface).String()
+		}
+		if reflect.TypeOf(iface) != reflect.TypeOf(s.output) {
+			fmt.Printf("Type mismatch, expected %s but got %s\n", reflect.TypeOf(s.output), reflect.TypeOf(iface))
+			t.Fail()
+			return
+		}
+		if iface != s.output {
+			fmt.Printf("Value mismatch expected %v but got %v\n", s.output, iface)
+			t.Fail()
+			return
+		}
+		fmt.Printf("Got %#v -> %s\n", iface, typ)
+	}
+}
+
+func TestParse_String(t *testing.T) {
+	var tests = []tst{
+		{"''", true, ""},
+		{"\"\"", true, ""},
+		{"'hello'", true, "hello"},
+		{"\"world\"", true, "world"},
+		{"'π'", true, "π"},
+		{"'it\\'s π day'", true, "it's π day" },
+		{"\"it's π day\"", true, "it's π day"},
+		{"'\"we\\'ll see about that \"'", true, "\"we'll see about that \""},
+		{"\"\\\"we'll see about that \\\"\"", true, "\"we'll see about that \""},
+		{"\"hello", false, nil},
+		{"'hello", false, nil},
+		{"hello\"", false, nil},
+		{"hello'", false, nil},
+		{"\"hello'", false, nil},
+		{"hello", false, nil},
+	}
+
+	for _, s := range tests {
+		t.Run(s.input, doTest(s, Entrypoint("String")))
+	}
+}
+
+func TestParse_UnquotedString(t *testing.T) {
+	var tests = []tst{
+		{"{{ hello }}", true, "{{ hello }}"},
+		{"{{ hello-world }}", true, "{{ hello-world }}"},
+		{"{{ h0la }}", true, "{{ h0la }}"},
+		{"{{hello }}", true, "{{hello }}"},
+		{"{{ hello}}", true, "{{ hello}}"},
+		{"{{	hello	}}", true, "{{	hello	}}"},
+		{"{{ hello", false, nil},
+		{"{{ hello }", false, nil},
+		{"{ hello }}", false, nil},
+	}
+
+	for _, s := range tests {
+		t.Run(s.input, doTest(s, Entrypoint("UnquotedString")))
+	}
+}
