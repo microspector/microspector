e49a90db192f9a2b1db60294d6e3147bcc0516fe
Tufan Baris Yildirim <tufanbarisyildirim@gmail.com>
2019-09-17 16:38:50 +0300

    init testing

--- go.mod
+++ go.mod
@@ -4,6 +4,9 @@ go 1.13
 
 require (
 	github.com/davecgh/go-spew v1.1.1
+	github.com/google/go-cmp v0.3.1 // indirect
 	github.com/mna/pigeon v1.0.0 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
 	github.com/thedevsaddam/gojsonq v2.2.1+incompatible
+	gotest.tools v2.2.0+incompatible
 )
---
+++ pkg/parser/scanner_test.go
@@ -0,0 +1,73 @@
+package parser
+
+import (
+	"gotest.tools/assert"
+	"testing"
+)
+
+func TestScanner_Set(t *testing.T) {
+
+	l := Parse(`SET {{ Domain }} "microspector.com"`)
+
+	assert.Assert(t, len(l.tokens) == 7)
+	assert.Equal(t, l.tokens[0].Type, SET)
+	assert.Equal(t, l.tokens[1].Type, int('{'))
+	assert.Equal(t, l.tokens[2].Type, int('{'))
+	assert.Equal(t, l.tokens[3].Type, IDENTIFIER)
+	assert.Equal(t, l.tokens[3].Text, "Domain")
+	assert.Equal(t, l.tokens[4].Type, int('}'))
+	assert.Equal(t, l.tokens[5].Type, int('}'))
+	assert.Equal(t, l.tokens[6].Type, STRING)
+	assert.Equal(t, l.tokens[6].Text, "microspector.com")
+
+}
+
+func TestScanner_Must(t *testing.T) {
+
+	l := Parse(`MUST {{ Domain }} EQUALS "microspector.com"`)
+
+	assert.Assert(t, len(l.tokens) == 8)
+	assert.Equal(t, l.tokens[0].Type, MUST)
+	assert.Equal(t, l.tokens[1].Type, int('{'))
+	assert.Equal(t, l.tokens[2].Type, int('{'))
+	assert.Equal(t, l.tokens[3].Type, IDENTIFIER)
+	assert.Equal(t, l.tokens[3].Text, "Domain")
+	assert.Equal(t, l.tokens[4].Type, int('}'))
+	assert.Equal(t, l.tokens[5].Type, int('}'))
+	assert.Equal(t, l.tokens[6].Type, EQUALS)
+	assert.Equal(t, l.tokens[7].Type, STRING)
+	assert.Equal(t, l.tokens[7].Text, "microspector.com")
+
+}
+
+func TestScanner_Debug(t *testing.T) {
+	l := Parse(`DEBUG {{ Domain }}`)
+
+	assert.Assert(t, len(l.tokens) == 6)
+	assert.Equal(t, l.tokens[0].Type, DEBUG)
+	assert.Equal(t, l.tokens[1].Type, int('{'))
+	assert.Equal(t, l.tokens[2].Type, int('{'))
+	assert.Equal(t, l.tokens[3].Type, IDENTIFIER)
+	assert.Equal(t, l.tokens[3].Text, "Domain")
+	assert.Equal(t, l.tokens[4].Type, int('}'))
+	assert.Equal(t, l.tokens[5].Type, int('}'))
+
+	l2 := Parse(`DEBUG {{ Domain }} "microspector.com" {{ Domain }}`)
+
+	assert.Assert(t, len(l2.tokens) == 12)
+	assert.Equal(t, l2.tokens[0].Type, DEBUG)
+	assert.Equal(t, l2.tokens[1].Type, int('{'))
+	assert.Equal(t, l2.tokens[2].Type, int('{'))
+	assert.Equal(t, l2.tokens[3].Type, IDENTIFIER)
+	assert.Equal(t, l2.tokens[3].Text, "Domain")
+	assert.Equal(t, l2.tokens[4].Type, int('}'))
+	assert.Equal(t, l2.tokens[5].Type, int('}'))
+	assert.Equal(t, l2.tokens[6].Type, STRING)
+	assert.Equal(t, l2.tokens[6].Text, "microspector.com")
+	assert.Equal(t, l2.tokens[7].Type, int('{'))
+	assert.Equal(t, l2.tokens[8].Type, int('{'))
+	assert.Equal(t, l2.tokens[9].Type, IDENTIFIER)
+	assert.Equal(t, l2.tokens[9].Text, "Domain")
+	assert.Equal(t, l2.tokens[10].Type, int('}'))
+	assert.Equal(t, l2.tokens[11].Type, int('}'))
+}
